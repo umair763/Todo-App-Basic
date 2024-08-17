7bf9621b72c3856f30ca046c17e3c9c969429de3
Muhammad Umair <93200938+umair763@users.noreply.github.com>
2024-08-17 11:00:23 +0500

    update

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './styles/AddTaskForm.css';
- 
+
 function AddTaskForm({ SetisAddFormVisible, addTask }) {
     const [color, setColor] = useState('');
     const [task, setTask] = useState('');
@@ -23,64 +23,49 @@ function AddTaskForm({ SetisAddFormVisible, addTask }) {
     function handleform(e) {
         e.preventDefault();
 
-        // Format the date string into Pakistani format
-        // const DateFormatted = formatDateToPakistani(date);
-
         const newTaskobj = {
             color,
             task,
-            date,
-            time,
+            date: formatDateToPakistani(date),
+            time: convertTo12HourFormat(time),
             status: false,
         };
 
-        console.log(newTaskobj);
-
-        if (color !== '' && task !== '' && date !== '' && time !== '') {
-            addTask(newTaskobj);
+        if (color && task && date && time) {
+            addTask(newTaskobj); // Send the new task to the parent function to handle the database addition
             setColor('');
             setTask('');
-            // setDate('');
-            // setTime('');
+            setDate('');
+            setTime('');
         }
     }
 
     return (
-        <>
-            <form className="addtask-form" onSubmit={handleform}>
-                <label>Choose color</label>
-                <select className="addentrysortby" value={color} onChange={(e) => setColor(e.target.value)}>
-                    <option>set color</option>
-                    <option>red</option>
-                    <option>yellow</option>
-                    <option>green</option>
-                </select>
+        <form className="addtask-form" onSubmit={handleform}>
+            <label>Choose color</label>
+            <select className="addentrysortby" value={color} onChange={(e) => setColor(e.target.value)}>
+                <option value="">Set color</option>
+                <option value="red">Red</option>
+                <option value="yellow">Yellow</option>
+                <option value="green">Green</option>
+            </select>
 
-                <label>Write task</label>
-                <input type="text" className="writeTask" value={task} onChange={(e) => setTask(e.target.value)} />
+            <label>Write task</label>
+            <input type="text" className="writeTask" value={task} onChange={(e) => setTask(e.target.value)} />
 
-                <label>Set Date</label>
-                <input
-                    type="date"
-                    // value={date}
-                    className="settime"
-                    onChange={(e) => setDate(formatDateToPakistani(e.target.value))}
-                />
+            <label>Set Date</label>
+            <input type="date" className="settime" value={date} onChange={(e) => setDate(e.target.value)} />
 
-                <label>Set time</label>
-                <input
-                    type="time"
-                    // value={time}
-                    className="settime"
-                    onChange={(e) => setTime(convertTo12HourFormat(e.target.value))}
-                />
+            <label>Set time</label>
+            <input type="time" className="settime" value={time} onChange={(e) => setTime(e.target.value)} />
 
-                <button className="confirmbtn">Confirm</button>
-                <button className="closebtn" onClick={SetisAddFormVisible}>
-                    Close
-                </button>
-            </form>
-        </>
+            <button className="confirmbtn" type="submit">
+                Confirm
+            </button>
+            <button className="closebtn" onClick={SetisAddFormVisible}>
+                Close
+            </button>
+        </form>
     );
 }
 
